11f6bab53ddc67bb4f1e36ec51f164fd2fc26508
zimsmith12 <zismi2@morgan.edu>
2025-11-10 10:12:39 -0500

    Update and rename saving_yaml to saving_yaml.py

---
+++ MSUTorch/examples_folder/saving_yaml.py
@@ -0,0 +1,4 @@
+Saving to YAML
+from MSUTorch.dataset.dataset_metadata import DatasetMetadata
+from MSUTorch.storage.yaml import save_dataset_yaml, load_dataset_yaml
+filepath = "./datasets/yaml_test_dataset"
