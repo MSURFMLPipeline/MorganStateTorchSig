8c617181c466774e2770363ca0eb545c55f55b05
Zimiego <zismi2@morgan.edu>
2025-11-14 11:31:43 +0000

    making yaml file

--- MSUTorch/examples_folder/saving_yaml.py
+++
@@ -1,5 +0,0 @@
-Saving to YAML
-from MSUTorch.dataset.dataset_metadata import DatasetMetadata
-from MSUTorch.storage.yaml import save_dataset_yaml, load_dataset_yaml
-filepath = "./datasets/yaml_test_dataset"
-save_dataset_yaml(filepath, dataset)
