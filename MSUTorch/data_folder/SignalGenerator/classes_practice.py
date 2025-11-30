c816c1530553a9a2e0e5ea58fa98559eb374a405
Zimiego Smith <zismi2@morgan.edu>
2025-11-30 16:07:06 -0500

    Practice with classes

---
+++ MSUTorch/data_folder/SignalGenerator/classes_practice.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import random
+"""Creating classes for simple sine wave"""
+class one:
+    waveform="sine" # attribute
+
+    def __init__(self,phase,data_points,frequency):
+        self.phase=phase 
+        self.data_points=data_points
+        self.frequency=frequency
+first_wave=one(random,random,random)
+second_wave=one(random,random,random)
+
+print(one.waveform) 
+print(first_wave.phase)
+print(second_wave.phase)
+print(second_wave.data_points)
+
+class amplitude(one):
+    def amplitude(self):
+        return f"Amplitude of the wave with frequency {self.frequency}Hz is 1.0"
+amp_wave=amplitude(0,100,440)
+print(amp_wave.amplitude())
