c612560241e998736c69f3aa6371067e7625c290
Zimiego Smith <zismi2@morgan.edu>
2025-11-30 16:51:22 -0500

    changes to code

--- MSUTorch/data_folder/SignalGenerator/classes_practice.py
+++ MSUTorch/data_folder/SignalGenerator/classes_practice.py
@@ -10,15 +10,15 @@ class one:
         self.phase=phase 
         self.data_points=data_points
         self.frequency=frequency
-first_wave=one(random,random,random)
-second_wave=one(random,random,random)
+first_wave=one(0,100,200)
+second_wave=one(0,100,200)
 
 print(one.waveform) 
 print(first_wave.phase)
 print(second_wave.phase)
 print(second_wave.data_points)
 
-class amplitude(one):
+class two(one):
     def amplitude(self):
         return f"Amplitude of the wave with frequency {self.frequency}Hz is 1.0"
 amp_wave=amplitude(0,100,440)
