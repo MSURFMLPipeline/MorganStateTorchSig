58ce00c0e4e75d80152a79dea292992b44ced334
Zimiego Smith <zismi2@morgan.edu>
2026-01-08 22:31:30 -0500

    Adding files from personal laptop

---
+++ MSUTorch/data_folder/generate_signal/Yaml_example.py
@@ -0,0 +1,22 @@
+from torchsig.utils.defaults import default_dataset
+from torchsig.utils.yaml import save_dataset_yaml, load_dataset_yaml
+
+filepath = "/home/vboxuser/MorganStateTorchSig/MSUTorch/generate_signal/Yaml_loaded.yaml"
+
+dataset = default_dataset(seed=42, target_labels=["class_name","snr_db"], impairment_level=None) # basic default dataset used for testing
+save_dataset_yaml(filepath, dataset)
+
+print(next(dataset))
+print(next(dataset))
+print(next(dataset))
+
+#Loading from YAML
+
+#Now we load a copy of the same dataset. Because the copy loads the same random seed from YAML, the values returned below should match the values above.
+
+dataset_copy = load_dataset_yaml(filepath)
+
+print(next(dataset_copy))
+print(next(dataset_copy))
+print(next(dataset_copy))
+
