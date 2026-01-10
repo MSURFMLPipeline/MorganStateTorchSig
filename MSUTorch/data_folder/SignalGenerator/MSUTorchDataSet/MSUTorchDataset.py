062ac54cd74b2286c33df56ba8553363ca159147
Zimiego Smith <zismi2@morgan.edu>
2026-01-10 17:05:23 -0500

    Making new file

--- MSUTorch/data_folder/SignalGenerator/MSUTorchDataSet/MSUTorchDataset.py
+++ MSUTorch/data_folder/SignalGenerator/MSUTorchDataSet/MSUTorchDataset.py
@@ -1,5 +1,15 @@
 """Generating MSUTORCH CLASSES"""
 import numpy as np
+import matplotlib.pyplot as plt
+from torchsig.datasets.dataset_metadata import DatasetMetadata
+from torchsig.datasets.datasets import TorchSigIterableDataset
+from torchsig.transforms.transforms import Spectrogram
+from torchsig.utils.writer import DatasetCreator, default_collate_fn
+from torchsig.utils.data_loading import WorkerSeedingDataLoader
+from tqdm.notebook import tqdm
+from torchsig.signals.signal_lists import TorchSigSignalLists
+from torchsig.datasets.datasets import StaticTorchSigDataset
+
 class MSUTorchDataset:
     def __init__(self,sample_rate,iq_samples,fft_size,snr_min,snr_max,min_signals,max_signals,class_index):
         self.sample_rate=sample_rate
@@ -11,4 +21,13 @@ class MSUTorchDataset:
         self.max_signals=max_signals
         self.class_index=class_index
 
-        
+    def modulation(self):
+        return TorchSigIterableDataset["class_index"]
+
+"""
+        dataset_time_series = TorchSigIterableDataset(
+dataset_metadata = dataset_time_series_metadata, 
+target_labels=["class_index"] """
+
+s1=MSUTorchDataset(90,10, 20, 10, 39, 40, 70, 9 )
+s1.modulation()
