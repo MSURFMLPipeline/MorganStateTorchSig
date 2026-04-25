bec189c92263ecc42b7e56449537b096019ab76c
Zimiego Smith <zismi2@morgan.edu>
2026-04-25 14:49:06 -0400

    Add functions for writing YOLO CPE datasets

--- MSUTorch/data_generation/transforms.py
+++ MSUTorch/data_generation/transforms.py
@@ -1,5 +1,130 @@
 
 
+
+      def writing_YOLO_CPE_Test_Dataset(self):
+
+        for i, (test_clean_emi_data, test_clean_emi_labels) in tqdm(
+            enumerate(self.testing_cpe_static_dataset),
+            total=len(self.testing_cpe_static_dataset),
+            desc="\nWriting YOLO Testing Dataset"):
+
+            file_base = str(i).zfill(10)
+            test_clean_emi_label_file = f"{self.clean_emi_test_label_dir}/{file_base}.txt"
+            test_clean_emi_img_file = f"{self.clean_emi_test_image_dir}/{file_base}.png"
+
+        # -------------------------
+        # Convert spectrogram to image
+        # -------------------------
+            test_clean_emi_img = test_clean_emi_data.detach().cpu().numpy() if hasattr(test_clean_emi_data, 'detach') else np.array(test_clean_emi_data)
+            test_clean_emi_img = np.squeeze(test_clean_emi_img).astype(np.float32)
+            test_clean_emi_img = np.abs(test_clean_emi_img)
+            test_clean_emi_img = np.log10(test_clean_emi_img + 1e-12) 
+            test_clean_emi_img = test_clean_emi_img - test_clean_emi_img.min()
+            test_clean_emi_img = np.nan_to_num(test_clean_emi_img, nan=0.0, posinf=0.0, neginf=0.0)
+            test_clean_emi_img = np.maximum(test_clean_emi_img, 1e-12)
+
+            if test_clean_emi_img.max() > 0:
+                test_clean_emi_img = test_clean_emi_img / test_clean_emi_img.max()
+                test_clean_emi_img = (test_clean_emi_img * 255).astype(np.uint8)
+                test_clean_emi_img = np.stack([test_clean_emi_img, test_clean_emi_img, test_clean_emi_img], axis=-1)
+                Image.fromarray(test_clean_emi_img).save(test_clean_emi_img_file)
+
+        # -------------------------
+        # Write YOLO label
+        # -------------------------
+            with open(test_clean_emi_label_file, "w") as f:
+                if isinstance(test_clean_emi_labels[0], (list, tuple, np.ndarray)):
+                    for l in test_clean_emi_labels:
+                        f.write(f"{int(l[0])} {l[1]} {l[2]} {l[3]} {l[4]}\n")
+                else:
+                    l = test_clean_emi_labels
+                    f.write(f"{int(l[0])} {l[1]} {l[2]} {l[3]} {l[4]}\n")
+
+    print("\nYOLO Testing Clean+EMI Dataset Generation Complete.")
+
+
+    def writing_YOLO_CPE_Validation_Dataset(self):
+
+        for i, (val_clean_emi_data, val_clean_emi_labels) in tqdm(
+            enumerate(self.validation_cpe_static_dataset),
+            total=len(self.validation_cpe_static_dataset),
+            desc="\nWriting YOLO Training Dataset"):
+
+            file_base = str(i).zfill(10)
+            val_clean_emi_label_file = f"{self.clean_emi_val_label_dir}/{file_base}.txt"
+            val_clean_emi_img_file = f"{self.clean_emi_val_image_dir}/{file_base}.png"
+
+        # -------------------------
+        # Convert spectrogram to image
+        # -------------------------
+            val_clean_emi_img = val_clean_emi_data.detach().cpu().numpy() if hasattr(val_clean_emi_data, 'detach') else np.array(val_clean_emi_data)
+            val_clean_emi_img = np.squeeze(val_clean_emi_img).astype(np.float32)
+            val_clean_emi_img = np.abs(val_clean_emi_img)
+            val_clean_emi_img = np.log10(val_clean_emi_img + 1e-12)
+            val_clean_emi_img = val_clean_emi_img - val_clean_emi_img.min()
+            val_clean_emi_img = np.maximum(val_clean_emi_img, 1e-12)
+            val_clean_emi_img = np.nan_to_num(val_clean_emi_img, nan=0.0, posinf=0.0, neginf=0.0)
+
+            if val_clean_emi_img.max() > 0:
+                val_clean_emi_img = val_clean_emi_img / val_clean_emi_img.max()
+                val_clean_emi_img = (val_clean_emi_img * 255).astype(np.uint8)
+                val_clean_emi_img = np.stack([val_clean_emi_img, val_clean_emi_img, val_clean_emi_img], axis=-1)
+                Image.fromarray(val_clean_emi_img).save(val_clean_emi_img_file)
+
+        # -------------------------
+        # Write YOLO label
+        # -------------------------
+            with open(val_clean_emi_label_file, "w") as f:
+                if isinstance(val_clean_emi_labels[0], (list, tuple, np.ndarray)):
+                    for l in val_clean_emi_labels:
+                        f.write(f"{int(l[0])} {l[1]} {l[2]} {l[3]} {l[4]}\n")
+                else:
+                    l = val_clean_emi_labels
+                    f.write(f"{int(l[0])} {l[1]} {l[2]} {l[3]} {l[4]}\n")
+
+    print("\nYOLO Validation Clean+EMI Dataset Generation Complete.")
+    
+    def writing_YOLO_CPE_Training_Dataset(self):
+
+        for i, (train_clean_emi_data, train_clean_emi_labels) in tqdm(
+            enumerate(self.training_cpe_static_dataset),
+            total=len(self.training_cpe_static_dataset),
+            desc="\nWriting YOLO Training Dataset"):
+
+            file_base = str(i).zfill(10)
+            train_clean_emi_label_file = f"{self.clean_emi_train_label_dir}/{file_base}.txt"
+            train_clean_emi_img_file = f"{self.clean_emi_train_image_dir}/{file_base}.png"
+
+        # -------------------------
+        # Convert spectrogram to image
+        # -------------------------
+            train_clean_emi_img = train_clean_emi_data.detach().numpy() if hasattr(train_clean_emi_data, 'detach') else np.array(train_clean_emi_data)
+            train_clean_emi_img = np.squeeze(train_clean_emi_img).astype(np.float32)
+            train_clean_emi_img = np.abs(train_clean_emi_img)
+            train_clean_emi_img = np.log10(train_clean_emi_img + 1e-12)
+            train_clean_emi_img = train_clean_emi_img - train_clean_emi_img.min()
+            train_clean_emi_img = np.maximum(train_clean_emi_img, 1e-12)
+            train_clean_emi_img = np.nan_to_num(train_clean_emi_img, nan=0.0, posinf=0.0, neginf=0.0)
+
+            if train_clean_emi_img.max() > 0:
+                train_clean_emi_img = train_clean_emi_img / train_clean_emi_img.max()
+                train_clean_emi_img = (train_clean_emi_img * 255).astype(np.uint8)
+                train_clean_emi_img = np.stack([train_clean_emi_img, train_clean_emi_img, train_clean_emi_img], axis=-1)
+                Image.fromarray(train_clean_emi_img).save(train_clean_emi_img_file)
+
+        # -------------------------
+        # Write YOLO label
+        # -------------------------
+            with open(train_clean_emi_label_file, "w") as f:
+                if isinstance(train_clean_emi_labels[0], (list, tuple, np.ndarray)):
+                    for l in train_clean_emi_labels:
+                        f.write(f"{int(l[0])} {l[1]} {l[2]} {l[3]} {l[4]}\n")
+                else:
+                    l = train_clean_emi_labels
+                    f.write(f"{int(l[0])} {l[1]} {l[2]} {l[3]} {l[4]}\n")
+
+    print("\nYOLO Training Clean+EMI Dataset Generation Complete.")  
+
     def writing_YOLO_emi_Training_Dataset(self):
 
         for i, (train_emi_data, train_emi_labels) in tqdm(
